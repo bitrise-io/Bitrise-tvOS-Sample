646295f28b41a590d6c8290673036ba9212aee03
Olivér Falvai <ofalvai@gmail.com>
2024-03-28 13:17:39 +0100

    Initial Commit

---
+++ Bitrise-tvOS-Sample/Bitrise_tvOS_SampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bitrise_tvOS_SampleApp.swift
+//  Bitrise-tvOS-Sample
+//
+//  Created by Olivér Falvai on 28/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct Bitrise_tvOS_SampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
